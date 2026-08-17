07fafe834c81bd78a0ca8d6e852db533d1d66bf2
Durgesh Gupta <durgs7642@gmail.com>
2026-08-17 22:59:07 +0530

    Time: 3 ms (74.59%), Space: 29.1 MB (14.74%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,17 +4,14 @@ public:
         int n = nums.size();
         stack<int> st;
         vector<int> res(n);
-        // st.push(nums[n-1]);
-        
-        for(int i = 2*n-1; i>=0;i--){
-            while(!st.empty() && st.top() <= nums[i%n]) st.pop();
-            if(i < n){
+        for(int i = n-2; i>=0;i--) st.push(nums[i]);
+
+        for(int i = n-1; i>=0;i--){
+            while(!st.empty() && st.top() <= nums[i]) st.pop();
             if(st.empty()) res[i] = -1;
             else res[i] = st.top();
-            }
-            st.push(nums[i%n]);
+            st.push(nums[i]);
         }
-
         return res;
     }
 };
