4ac1bd29ea465021ea523f2af19b57e940610fa0
Durgesh Gupta <durgs7642@gmail.com>
2026-09-21 17:48:31 +0530

    Time: 325 ms (5.05%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,6 @@
 -- Write your PostgreSQL query statement below
-SELECT
-(
-SELECT DISTINCT salary
-FROM
-(SELECT salary, DENSE_RANK() OVER(ORDER BY salary DESC) as rank
-FROM Employee
-) AS RankedSalaries
-WHERE rank = 2
-) AS Secondhighestsalary ;
+SELECT (
+    SELECT DISTINCT salary FROM Employee
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1
+) AS Secondhighestsalary;
